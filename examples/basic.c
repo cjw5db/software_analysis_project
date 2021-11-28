193dbb98cc4eeb88d8a2f094bbd04bb89f0b32f7
Conner Ward <connerward15@gmail.com>
2021-11-28 00:14:24 -0500

    fix spacing for examples/basic.c

--- examples/basic.c
+++ examples/basic.c
@@ -5,17 +5,17 @@
 #define RES 16
 
 int main(){
-	int a;
-	uint8_t arr[LEN];
-	klee_make_symbolic(&arr, sizeof(arr), "Array");
+  int a;
+  uint8_t arr[LEN];
+  klee_make_symbolic(&arr, sizeof(arr), "Array");
 
-	for(int i = 0; i < LEN; i++){
+  for(int i = 0; i < LEN; i++){
     klee_assume(arr[i] > 0);
-	}
+  }
 
   if(arr[0] == RES){
     return 1;
   }
 
-	return 0;
+  return 0;
 }
