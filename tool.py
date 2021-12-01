5a0a50090943b0a11b727fe182115f415e93b6bd
Conner Ward <cjw5db@virginia.edu>
2021-12-01 19:10:08 +0000

    identify dominance edges, back edges, and loops in CFG

--- tool.py
+++ tool.py
@@ -3,6 +3,7 @@ import subprocess
 import re
 import os
 
+#argv is an arrar of command line inputs
 #validate command line input
 if len(sys.argv) < 2:
     print("Command line arguments are required")
@@ -17,21 +18,20 @@ else:
             exit()
 
 #generate control flow graph
-command = "clang -c -Xclang -analyze -Xclang -analyzer-checker=debug.DumpCFG "
-output = subprocess.check_output((command + sys.argv[1]).split(), stderr=subprocess.STDOUT)
+cfg_command = "clang -c -Xclang -analyze -Xclang -analyzer-checker=debug.DumpCFG "
+output = subprocess.check_output((cfg_command + sys.argv[1]).split(), stderr=subprocess.STDOUT)
 cfg = output.decode('UTF-8')
-
 
 #setup data structure
 functions = {}
 function = ""
 block = ""
 for line in cfg.splitlines():
-    line = line.strip()
+    line = line.strip() #removes whitespace on either end of line
     if len(line) == 0:
         continue
     if bool(re.match("\[", line)):
-        block = re.search("B\d+", line)[0]
+        block = re.search("\d+", line)[0]
         functions[function][block] = {}
     elif bool(re.match("\d+", line)):
         elements = line.split(":", maxsplit=1)
@@ -40,19 +40,74 @@ for line in cfg.splitlines():
         elements = line.split(":", maxsplit=1)
         functions[function][block]["T"] = elements[1].strip()
     elif bool(re.match("Preds", line)):
-        functions[function][block]["Preds"] = re.findall("B\d", line);
+        elements = line.split(":", maxsplit=1)
+        functions[function][block]["Preds"] = re.findall("\d+", elements[1]);
     elif bool(re.match("Succs", line)):
-        functions[function][block]["Succs"] = re.findall("B\d", line);
+        elements = line.split(":", maxsplit=1)
+        functions[function][block]["Succs"] = re.findall("\d+", elements[1]);
     else:
         function = line
         functions[function] = {}
 
+#generate dominance tree
+dom_command = "clang -c -Xclang -analyze -Xclang -analyzer-checker=debug.DumpDominators "
+output = subprocess.check_output((dom_command + sys.argv[1]).split(), stderr=subprocess.STDOUT)
+dom = output.decode('UTF-8')
+
+#store dominance edges in data structure
+funcs_iter = iter(functions.keys())
+for line in dom.splitlines():
+    if not line.startswith("("):
+        func = next(funcs_iter)
+        continue
+    edge = re.findall("\d+", line)
+    functions[func][edge[0]]["Dom"] = edge[1]
+
+#find and store back edges
+for function, blocks in functions.items():
+    blocks["Backs"] = {}
+    for block, lines in blocks.items():
+        if "Succs" in lines:
+            for succ in lines["Succs"]:
+                prev = block
+                dom = blocks[prev]["Dom"]
+                dominated = False
+                while dom != prev:
+                    if dom == succ:
+                        dominated = True
+                    prev = dom
+                    dom = blocks[prev]["Dom"]
+                if dominated and succ > block:
+                    blocks["Backs"][block] = succ
+
+#find and store loops in CFG
+for function, blocks in functions.items():
+    blocks["Loops"] = []
+    for n, d in blocks["Backs"].items():
+        loop = [n, d]
+        stack = [n]
+        while len(stack) != 0:
+            print(stack)
+            m = stack.pop()
+            if "Preds" not in blocks[m]:
+                continue
+            for pred in blocks[m]["Preds"]:
+                if pred not in loop:
+                    loop.append(pred)
+                    stack.append(pred)
+            print(stack)
+        blocks["Loops"].append(loop)
 
 #print data structure
-for function in functions:
+for function, blocks in functions.items():
     print(function)
-    for block in functions[function]:
+    for block, lines in blocks.items():
         print("\t" + block)
-        for statement in functions[function][block]:
-            print("\t\t" + statement + ":" + str(functions[function][block][statement]))
+        if isinstance(lines, list):
+            for item in lines:
+                print("\t\t", end = "")
+                print(*item, sep = ", ")
+        else:
+            for type, data in lines.items():
+                print("\t\t" + type + ":" + str(data))
 
