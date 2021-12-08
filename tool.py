05a192143af34190087a17718dd5cb88d3227398
Conner Ward <cjw5db@virginia.edu>
2021-12-08 21:34:32 +0000

    clean up code and add color coding to tool output

--- tool.py
+++ tool.py
@@ -3,6 +3,17 @@ import subprocess
 import re
 import os
 
+class bcolors:
+    PURPLE = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 class CFG:
     def __init__(self, cfg_str):
         self.cfg = {}
@@ -30,6 +41,41 @@ class CFG:
             else:
                 function = line
                 self.cfg[function] = {}
+        for function, blocks in self.cfg.items():
+            self.cfg[function] = dict(sorted(blocks.items(), key=lambda kv: int(kv[0]), reverse=True))
+
+    def __str__(self):
+        #print data structure
+        string = ""
+        for function, blocks in self.cfg.items():
+            string += (function + "\n")
+            for block, lines in blocks.items():
+                if "Backs" == block or "Loops" == block:
+                    string += bcolors.GREEN
+                else:
+                    string += bcolors.YELLOW
+                string += ("  " + block + "\n")
+                if "Backs" != block and "Loops" != block:
+                    string += bcolors.ENDC
+                if isinstance(lines, list):
+                    for item in lines:
+                        string += ("    " + str(item) + "\n")
+                elif isinstance(lines, dict):
+                    for key, value in lines.items():
+                        if "Dom" == key:
+                            string += bcolors.RED
+                        elif "T" == key:
+                            string += bcolors.CYAN
+                        elif "Preds" == key:
+                            string += bcolors.PURPLE
+                        elif "Succs" == key:
+                            string += bcolors.PURPLE
+                        string += ("    " + key + ":" + str(value) + "\n")
+                else:
+                    string += ("    " + lines + "\n")
+                string += bcolors.ENDC
+        return string
+
 
     def add_dominance_edges(self, dom_str):
         #store dominance edges in data structure
@@ -46,23 +92,27 @@ class CFG:
         for function, blocks in self.cfg.items():
             blocks["Backs"] = {}
             for block, lines in blocks.items():
-                if "Succs" in lines:
-                    for succ in lines["Succs"]:
-                        prev = block
+                if "Succs" not in lines:
+                    continue
+                for succ in lines["Succs"]:
+                    prev = block
+                    dom = blocks[prev]["Dom"]
+                    dominated = False
+                    while dom != prev:
+                        if dom == succ:
+                            dominated = True
+                            break
+                        prev = dom
                         dom = blocks[prev]["Dom"]
-                        dominated = False
-                        while dom != prev:
-                            if dom == succ:
-                                dominated = True
-                            prev = dom
-                            dom = blocks[prev]["Dom"]
-                        if dominated and succ > block:
-                            blocks["Backs"][block] = succ
+                    if dominated and succ > block:
+                        blocks["Backs"][block] = succ
 
     def calculate_loops(self):
         #find and store loops in CFG
         for function, blocks in self.cfg.items():
             blocks["Loops"] = []
+            if "Backs" not in blocks:
+                continue
             for n, d in blocks["Backs"].items():
                 loop = [n, d]
                 stack = [n]
@@ -74,24 +124,9 @@ class CFG:
                         if pred not in loop:
                             loop.append(pred)
                             stack.append(pred)
+                loop.sort(reverse=True)
                 blocks["Loops"].append(loop)
-
-    def __str__(self):
-        #print data structure
-        string = ""
-        for function, blocks in self.cfg.items():
-            string += (function + "\n")
-            for block, lines in blocks.items():
-                string += ("\t" + block + "\n")
-                if isinstance(lines, list):
-                    for item in lines:
-                        string += ("\t\t" + ", ".join(item) + "\n")
-                elif isinstance(lines, dict):
-                    for key, value in lines.items():
-                        string += ("\t\t" + key + ":" + str(value) + "\n")
-                else:
-                    string += ("\t\t" + lines + "\n")
-        return string
+                blocks["Loops"].sort(key=lambda loop: len(loop))
 
 if __name__ == "__main__":
     #validate command line input
